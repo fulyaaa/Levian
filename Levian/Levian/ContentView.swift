9d27bb7707fb62128b1c056740fd37643126ed11
Fulya Akan <fulyaakan@outlook.com>
2026-09-21 16:12:26 +0300

    Add Xcode project files

---
+++ Levian/Levian/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Levian
+//
+//  Created by fulya akan on 21.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Levian/Levian/LevianApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevianApp.swift
+//  Levian
+//
+//  Created by fulya akan on 21.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct LevianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
